d743cb53b45ffdc697973887f25523d2fc76860a
Fabio Rodrigues <fabiordrig@gmail.com>
2019-12-04 07:59:32 -0300

    ajustando webpack novamente

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,32 @@
-// webpack.config.js
-'use strict';
-
-const JavaScriptObfuscator = require('webpack-obfuscator');
-
+const path = require('path')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const dist = path.join(__dirname,"./dist")
 module.exports = {
-    entry: {
-        'abc': './src/main.js'
-    },
-    output: {
-        path: 'dist',
-        filename: 'teste.js'
-    },
-    plugins: [
-        new JavaScriptObfuscator({
-            rotateUnicodeArray: true
-        }, ['abc.js'])
-    ]
+  entry: {
+    "bundle": "./src/main.js",
+    "bundle.min": "./src/main.js",
+  },
+  target:'node',
+  devtool: "source-map",
+  output: {
+    path: dist,
+    filename: "[name].js"
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          warnings: false,
+          parse: {},
+          compress: {},
+          mangle: true, // Note `mangle.properties` is `false` by default.
+          output: null,
+          toplevel: false,
+          nameCache: null,
+          ie8: false,
+          keep_fnames: false,
+        },
+      }),
+    ],
+  },
 };
